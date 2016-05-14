34dad086fb1a393a16fda7ce442cca8ea3b55bb5
Scanniza <simon@cymonit.se>
2016-05-14 16:28:35 +0200

    like/dislike

--- FoodFeed/feed.js
+++ FoodFeed/feed.js
@@ -54,6 +54,49 @@ class Feed extends Component {
         }
       });
   }
+  likeRecipe (recipe) {
+    recipe.HasLiked = true;
+    recipe.Likes++;
+    this.forceUpdate();
+    AsyncStorage.getItem('loginToken')
+      .then((token) => {
+        if (token) {
+          var REQUEST_URL = BASE_URL + "/api/recipe/"+recipe.Id+"/like";
+          myInit = {
+            method:"POST",
+            headers:{"Authorization":"bearer "+token}
+          }
+          fetch(REQUEST_URL,myInit)
+            .then((response) => {
+            })
+            .done();
+        } else {
+          resolve({loggedIn: false});
+        }
+      });
+  }
+  dislikeRecipe (recipe) {
+    recipe.HasLiked = false;
+    recipe.Likes--;
+    this.forceUpdate();
+    AsyncStorage.getItem('loginToken')
+      .then((token) => {
+        if (token) {
+          var REQUEST_URL = BASE_URL + "/api/recipe/"+recipe.Id+"/unlike";
+          myInit = {
+            method:"POST",
+            headers:{"Authorization":"bearer "+token}
+          }
+          fetch(REQUEST_URL,myInit)
+            .then((response) => {
+            })
+            .done();
+        } else {
+          resolve({loggedIn: false});
+        }
+      });
+      
+  }
   render() {
     if (!this.state.loaded) {
       return this.renderLoadingView();
@@ -79,9 +122,11 @@ class Feed extends Component {
     return(
       <View style={styles.container}>
         <View style={styles.topContainer}>
-          <Text style={styles.username}>Hej</Text>
+          <Text style={styles.username}>{recipe.User.Name}</Text>
           <View style={styles.likeBox}>
-            <Text style={styles.likeHeart}><Icon name={recipe.HasLiked?'ios-heart':'ios-heart-outline'} color="#12311C" size={25} /></Text>
+            <TouchableOpacity onPress={()=>{recipe.HasLiked?this.dislikeRecipe(recipe):this.likeRecipe(recipe)}}>
+              <Text style={styles.likeHeart}><Icon name={recipe.HasLiked?'ios-heart':'ios-heart-outline'} color="#12311C" size={25} /></Text>
+            </TouchableOpacity>
             <Text style={styles.likeCount}> {recipe.Likes}</Text>
           </View>
         </View>
